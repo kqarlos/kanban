672501337b5960ed0e385dbfcb6f7f8813fd8dc5
kqarlos <kqarlos@hotmail.com>
2021-01-15 00:02:10 -0800

    uses form to collect task data to render

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,25 @@ const formEl = document.querySelector("#task-form");
 const tasksToDoEl = document.querySelector("#tasks-to-do");
 
 function createTaskHandler(e) {
-    e.preventDefault()
-    let taskItemEl = document.createElement("li");
-    taskItemEl.classList.add("task-item")
-    taskItemEl.textContent = "New Task";
+    e.preventDefault();
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    // Create list item
+    let listItemEl = document.createElement("li");
+    listItemEl.classList.add("task-item");
+    // Create div holder
+    let taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    // Add html content
+    taskInfoEl.innerHTML =
+        `<h3 class='task-name'>${taskNameInput}</h3>
+        <span class='task-type'>${taskTypeInput}</span>`;
 
-    tasksToDoEl.appendChild(taskItemEl);
+
+    listItemEl.appendChild(taskInfoEl);
+    // Add list item to list
+    tasksToDoEl.appendChild(listItemEl);
 }
 
 formEl.addEventListener("submit", createTaskHandler);
