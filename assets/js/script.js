808d49458a02b21263f806984cc4b3cc8dd4d759
kqarlos <kqarlos@hotmail.com>
2021-01-19 12:04:27 -0800

    Adds comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ let tasks = [];
 // Handles task form submission
 function taskFormHandler(e) {
     e.preventDefault();
+    //Gets name and type from form
     let taskNameInput = document.querySelector("input[name='task-name']").value;
     let taskTypeInput = document.querySelector("select[name='task-type']").value;
 
@@ -20,13 +21,12 @@ function taskFormHandler(e) {
 
     formEl.reset();
 
+    //Check if form input is an edit or a new task, render and update accordingly
     let isEdit = formEl.hasAttribute("data-task-id");
-
     let taskData = {
         name: taskNameInput,
         type: taskTypeInput,
     }
-
     if (isEdit) {
         taskData.taskId = formEl.getAttribute("data-task-id");;
         completeEditTask(taskData);
@@ -34,30 +34,29 @@ function taskFormHandler(e) {
         taskData.status = "to do";
         renderTask(taskData);
     }
-
 }
 
 //Renders a task to the page
 function renderTask(taskData) {
-    // Create list item
+    // Create task list item
     let taskItemEl = document.createElement("li");
     taskItemEl.classList.add("task-item");
     taskItemEl.setAttribute("data-task-id", taskIdCounter);
     taskItemEl.setAttribute("draggable", "true");
 
-    // Create div holder
+    // Create task info elements
     let taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-    // Add task name and type
     taskInfoEl.innerHTML =
         `<h3 class='task-name'>${taskData.name}</h3>
             <span class='task-type'>${taskData.type}</span>`;
 
-    // Add content to list item holder and list
+    // Add info and actions elements to task list item
     taskItemEl.appendChild(taskInfoEl);
     let taskActionsEl = createTaskActions(taskIdCounter);
     taskItemEl.appendChild(taskActionsEl);
 
+    //Check status render in the appropiate container element 
     switch (taskData.status) {
         case ("to do"):
             taskActionsEl.querySelector("select[name='status-change']").selectedIndex = 0;
@@ -73,21 +72,21 @@ function renderTask(taskData) {
             break;
     }
 
-
-
-
-    //Update task data object
+    //Update task data with ID
     taskData.id = taskIdCounter;
+    // Update tasks list and local storage
     tasks.push(taskData);
     saveTasks();
+
     taskIdCounter++;
 }
 
+// Creates acton eleemetns for a task
 function createTaskActions(taskId) {
     let actionsContainerEl = document.createElement("div");
     actionsContainerEl.className = "task-actions";
 
-    //Create and append action buttons
+    //Create and append edit and delete buttons
     let editBtnEl = document.createElement("button");
     editBtnEl.textContent = "Edit";
     editBtnEl.className = "btn edit-btn";
@@ -107,7 +106,6 @@ function createTaskActions(taskId) {
     statusSelectEl.setAttribute("data-task-id", taskId);
     actionsContainerEl.appendChild(statusSelectEl);
 
-
     let statusChoices = ["To Do", "In Progress", "Completed"];
     for (let i = 0; i < statusChoices.length; i++) {
         // create and append option element
@@ -116,12 +114,12 @@ function createTaskActions(taskId) {
         statusOptionEl.setAttribute("value", statusChoices[i]);
         statusSelectEl.appendChild(statusOptionEl);
     }
-    // actionsContainerEl.appendChild(statusSelectEl);
     return actionsContainerEl;
-
 }
 
+// Deletes a task from the task list
 function deleteTask(taskId) {
+    //Get task using the taskID and rmeove it from the DOM
     let task = document.querySelector(`.task-item[data-task-id='${taskId}']`);
     task.remove();
 
@@ -134,9 +132,11 @@ function deleteTask(taskId) {
         }
     }
     tasks = updatedTaskArr;
+    // Update tasks in localStorage
     saveTasks();
 }
 
+// Edit a task. Gets task info and displays it on the form to be edited
 function editTask(taskId) {
     let task = document.querySelector(`.task-item[data-task-id='${taskId}']`);
     let taskName = task.querySelector("h3.task-name").textContent;
@@ -148,11 +148,13 @@ function editTask(taskId) {
     formEl.setAttribute("data-task-id", taskId);
 }
 
+// renders updated task info
 function completeEditTask({ name, type, taskId }) {
+    // GEts task id and inf
     let task = document.querySelector(`.task-item[data-task-id='${taskId}']`)
     task.querySelector("h3.task-name").textContent = name;
     task.querySelector("span.task-type").textContent = type;
-    //update task arrays
+    //update task arrays and local storage
     for (let i = 0; i < tasks.length; i++) {
         if (tasks[i].id === parseInt(taskId)) {
             tasks[i].name = name;
@@ -160,11 +162,13 @@ function completeEditTask({ name, type, taskId }) {
         }
     }
     saveTasks();
+    // resets form
     formEl.removeAttribute("data-task-id");
     document.querySelector("#save-task").textContent = "Add Task";
     alert("Task Updated!");
 }
 
+// Checks the if the event target is a button and calls its respective handler
 function taskButtonHandler(e) {
     let targetEl = e.target;
     if (targetEl.matches(".delete-btn")) {
@@ -176,6 +180,7 @@ function taskButtonHandler(e) {
     }
 }
 
+// Gets task to be updated and appends it to it's new section
 function taskStatusChangeHandler(e) {
     // Get task id, selected status option and task
     let taskId = e.target.getAttribute("data-task-id");
@@ -190,7 +195,7 @@ function taskStatusChangeHandler(e) {
         tasksCompletedEl.appendChild(task);
     }
 
-    // update task's in tasks array
+    // update task's in tasks array and local storage
     for (let i = 0; i < tasks.length; i++) {
         if (tasks[i].id === parseInt(taskId)) {
             tasks[i].status = statusValue;
@@ -199,6 +204,7 @@ function taskStatusChangeHandler(e) {
     saveTasks();
 }
 
+// Save task id to dataTransfer is element being drag is a task list item
 function dragTaskHandler(e) {
     if (e.target.matches("li.task-item")) {
         let taskId = e.target.getAttribute("data-task-id");
@@ -206,8 +212,9 @@ function dragTaskHandler(e) {
     }
 }
 
+// Change section background when hovering over with dragged task list item
 function dropZoneDragHandler(e) {
-    // If target is within a task list
+    // If target is within a task section
     let taskListEl = e.target.closest(".task-list");
     if (taskListEl) {
         e.preventDefault();
@@ -215,10 +222,10 @@ function dropZoneDragHandler(e) {
     }
 }
 
+// Handles status change when dropping task list item
 function dropTaskHandler(e) {
     e.preventDefault();
-
-    // Get id and draggaed element
+    // Get id and dragged element
     let id = e.dataTransfer.getData("text/plain");
     let draggableElement = document.querySelector(`[data-task-id='${id}']`);
     // Get dropzone element
@@ -236,11 +243,12 @@ function dropTaskHandler(e) {
     else if (statusType === "tasks-completed") {
         statusSelectEl.selectedIndex = 2;
     }
+    // Revert section background style
     dropZoneEl.removeAttribute("style");
     // Add task to drop zone
     dropZoneEl.appendChild(draggableElement);
 
-    // update task's in tasks array
+    // update task's in tasks array and localStorage
     for (let i = 0; i < tasks.length; i++) {
         if (tasks[i].id === parseInt(id)) {
             tasks[i].status = statusSelectEl.value.toLowerCase();
@@ -249,7 +257,7 @@ function dropTaskHandler(e) {
     saveTasks();
 }
 
-// Remove styling of task list element when dragLeave
+// Remove styling of task list section when dragLeave
 function dragLeaveHandler(e) {
     let taskListEl = e.target.closest(".task-list");
     if (taskListEl) {
@@ -257,10 +265,12 @@ function dragLeaveHandler(e) {
     }
 }
 
+// Save task list to localStorage
 function saveTasks() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+// Load tasks from localStorage
 function loadTasks() {
     var savedTasks = (JSON.parse(localStorage.getItem("tasks")) || []);
     savedTasks.forEach(task => {
@@ -268,6 +278,7 @@ function loadTasks() {
     });
 }
 
+// Event Listeners
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
@@ -276,4 +287,5 @@ pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
 pageContentEl.addEventListener("dragleave", dragLeaveHandler);
 
+// Load tasks upon loading
 $(loadTasks()); 
